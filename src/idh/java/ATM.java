4b801544c56fcf430614f87e789f70c52552b1b4
siegfried <nils.reiter@uni-koeln.de>
2026-06-03 13:05:15 +0200

    less formal

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -23,7 +23,7 @@ public class ATM  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Wieviel Geld möchten Sie abheben?");
+				System.out.print("Wieviel Kohle möchten Sie abheben?");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
 			} catch (Exception e) {
